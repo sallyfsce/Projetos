bb0ebe879e7ea886be4bbed42aed3f5d1d952ea4
sallyfsce <aneliza875@gmail.com>
2026-09-19 02:26:01 -0300

    janela inicial do conversor

---
+++ conversor.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+from tkinter import ttk
+
+janela = tk.Tk()
+janela.title("Conversor de Unidades")
+janela.geometry("450x400")
+janela.resizable(False, False)
+
+janela.mainloop()
